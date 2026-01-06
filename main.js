1b42dd81b2587fcade7000713fe07518359d8723
Topprogrammer65 <topprogrammer65@gmail.com>
2026-01-05 16:59:32 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+let clear = document.querySelector("#clear-btn");
+let equal = document.querySelector(".equal");
+let decimal = document.querySelector(".decimal");
 
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
     
-}
+})
